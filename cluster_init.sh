3a652eaf9dbf76724748da1c1b6b977d153a5429
billkellett <44028163+billkellett@users.noreply.github.com>
2021-03-04 10:53:13 -0500

    Add files via upload

---
+++ cluster_init.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "Beginning installation of external libraries for Linear Regression Workshop"
+pip install -r pip install -r /dbfs/FileStore/python/lrwkshp/requirements.txt
+echo "Library installation complete"
